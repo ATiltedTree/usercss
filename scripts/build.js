3bf048193be51fd44fc243f1d1a0c56d988bd8b7
Tilmann Meyer <allescrafterx@gmail.com>
2020-05-18 23:58:14 +0200

    refactor(build): Use foreach

--- scripts/build.js
+++ scripts/build.js
@@ -3,13 +3,12 @@ const fs = require('fs');
 
 const builds = ['github-wide', 'archlinux-dark'];
 
-for (let i = 0; i < builds.length; i++) {
-  const element = builds[i];
+builds.forEach((build) => {
   const result = sass.renderSync({
-    file: `src/${element}.user.scss`,
-    outFile: `${element}.user.css`,
+    file: `src/${build}.user.scss`,
+    outFile: `${build}.user.css`,
     outputStyle: 'expanded',
   });
-  fs.writeFileSync(`${element}.user.css`, result.css);
-  console.log(`Built ${element} in ${Math.floor(result.stats.duration / 1000)}s`);
-}
+  fs.writeFileSync(`${build}.user.css`, result.css);
+  console.log(`Built ${build} in ${result.stats.duration}ms.`);
+});
